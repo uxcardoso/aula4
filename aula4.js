79010f0d7fa3a6c3db5a71823e6f517fe246dfe8
Thiago Cardoso <thiago.elementos@gmail.com>
2024-06-29 00:18:06 -0300

    Desafio complementar no final da aula 4

--- aula4.js
+++ aula4.js
@@ -1,50 +1,7 @@
-function divisao() {
-    const num1 = parseInt(prompt('Digite um número'))
-    const num2 = parseInt(prompt('Digite outro número'))
-
-    if (num1 === 0 || num2 === 0) {
-        console.log("O filha da puta, Não é possível dividir o número por 0")
-    } else if (num1 > num2) {
-        console.log(num1 / num2);
-    } else if (num1 < num2) {
-        console.log(num2 / num1);
-    } 
-    
+function infoProd() {
+    let produtoPreco = parseInt(prompt('Informe o valor do produto R$'));
+    let icms = 0.21; 
+    let precoImposto = produtoPreco * icms; 
+    let valorTotal = produtoPreco + precoImposto; 
+    return console.log(`O valor com imposto é R$ ${valorTotal}`)
 }
-
-/*function calculadora (num1, num2, operador) {
-
-    if (operador === '+') {
-        return num1 + num2
-    } else if (operador === '-') {
-        return num1 - num2
-    } else if (operador === '*') {
-        return num1 * num2
-    } else if (operador === '/') {
-        return num1 / num1
-    } else {
-        return 0
-    }
-
-}
-
-console.log(calculadora(10, 3, '+'))
-console.log(calculadora(5, 3, '*'))
-*/
-
-/* 
-function mostra() {
-    const numero = parseInt(prompt('Digite um número'))
-
-        if (numero % 2 == 0) {
-            alert(`O número ${numero} é par`);
-        } else {
-            alert(`O número ${numero} é impar`);
-        }
-} 
-
-mostra()
-mostra()
-mostra()
-
-*/
---
+++ exercicios.js
@@ -0,0 +1,169 @@
+
+
+
+
+
+
+
+
+
+/*const precoProd = parseInt(prompt('Insira o valor do produto R$')) 
+
+const icms = 0.21 
+
+function calcImposto(precoProd, icms) {
+    const impostoCalculado = precoProd * icms;
+    const valorComImposto = precoProd + impostoCalculado;
+    return console.log(`O valor com imposto é R$ ${valorComImposto}`)
+    }
+
+calcImposto(precoProd, icms)
+
+    
+
+
+
+
+
+
+
+/* const soma = (num1, num2) => {
+    return num1 + num2
+}
+
+const subtrai = (num1, num2) => {
+    return num1 - num2
+}
+
+const multiplica = (num1, num2) => {
+    return num1 * num2
+}
+
+const divide = (num1, num2) => {
+    return num1 / num2
+}
+
+const calculadora = (num1, num2, operador) => {
+
+    if(operador === '+') {
+       return soma(num1, num2)
+    } else if(operador === '-') {
+        return subtrai(num1, num2)
+    } else if(operador === '*') {
+        return multiplica(num1, num2)
+    } else if(operador === '/') {
+        return divide(num1, num2)
+    } else {
+        return 0
+    }
+}
+
+console.log(calculadora(10, 10, '*'))
+
+
+
+/*const recebeInfo = () => {
+    return parseInt(prompt('Informe sua idade:'))
+}
+
+const calcAnoNascimento = (idade, ano) => {
+    return ano - idade
+}
+
+const mostraMensagem = (nascimento) => {
+    console.log(`Você nasceu em ${nascimento}`)
+}
+
+const idade = recebeInfo()
+const anoNascimento = calcAnoNascimento(idade, 2024)
+mostraMensagem(anoNascimento)
+
+
+
+
+/*const recebeInformacoes = () => {
+    const nome = prompt("Informe o nome do usuário")
+    const idade = parseInt(prompt('Informe a idade do usuário'))
+
+    return nome + idade
+}
+
+console.log(recebeInformacoes())
+
+
+
+/* const calculadora = function (num1, num2, operador) {
+
+}
+
+// preço do produto + ICMS 
+const soma = (preco, icms) => {
+    return preco + icms
+}
+
+
+const subtrair = (soma, desconto) => {
+    return soma - desconto
+}
+
+const calculaICMS = (preco) => {
+    return preco * 0.21
+}
+
+const icms = calculaICMS(50)
+const precoICMS = soma(50, icms)
+const precoDesconto = subtrair(precoICMS, 5)
+
+console.log(precoDesconto)
+
+
+// function divisao() {
+    // const num1 = parseInt(prompt('Digite um número'))
+    // const num2 = parseInt(prompt('Digite outro número'))
+// 
+    // if (num1 === 0 || num2 === 0) {
+        // console.log("Não é possível dividir o número por 0")
+    // } else if (num1 > num2) {
+        // console.log(num1 / num2);
+    // } else if (num1 < num2) {
+        // console.log(num2 / num1);
+    // } 
+    
+// }
+
+/*function calculadora (num1, num2, operador) {
+
+    if (operador === '+') {
+        return num1 + num2
+    } else if (operador === '-') {
+        return num1 - num2
+    } else if (operador === '*') {
+        return num1 * num2
+    } else if (operador === '/') {
+        return num1 / num1
+    } else {
+        return 0
+    }
+
+}
+
+console.log(calculadora(10, 3, '+'))
+console.log(calculadora(5, 3, '*'))
+*/
+
+/* 
+function mostra() {
+    const numero = parseInt(prompt('Digite um número'))
+
+        if (numero % 2 == 0) {
+            alert(`O número ${numero} é par`);
+        } else {
+            alert(`O número ${numero} é impar`);
+        }
+} 
+
+mostra()
+mostra()
+mostra()
+
+*/
